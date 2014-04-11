512e7223c54bcfa70f04cec104ef0fb4dbd1dc50
Steven Finley <steve.a.finley@gmail.com>
2014-04-11 15:37:44 -0500

    Item done

---
+++ lib/linked_list.rb
@@ -0,0 +1,10 @@
+require 'linked_list_item.rb'
+class LinkedList
+  def add_item (payload)
+    LinkedListItem.new(payload)
+  end
+
+  def get (num)
+
+  end
+end
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,5 +1,24 @@
 class LinkedListItem
-  def initialize(item)
+  include Comparable
+  attr_accessor :payload, :next_list_item
+  
+  def initialize(payload)
+    @payload = payload
+  end
 
+  def next_list_item= next_list_item
+    next_list_item == self ? (raise ArgumentError.new("Nope")) : (@next_list_item = next_list_item)
+  end
+
+  def last?
+    self.next_list_item == nil ? true : false
+  end
+
+  def ===(other)
+    self.object_id === other.object_id ? true : false
+  end
+
+  def <=> other
+    self.payload.to_s <=> other.payload.to_s
   end
 end
